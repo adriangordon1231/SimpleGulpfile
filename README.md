7924a7867eb66ca58560fd4394daeebe0775e575
Adrian Gordon <adriangordon@outlook.com>
2015-01-11 19:40:15 -0800

    added processes got building sass and compressing images and js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SimpleGulpfile
 A basic gulpfile that can be used to build simple projects.
+
+#Compress-Images
+Compresses images stored in img-tmp folder and stores them in the assets folder 
+
+#Build-Scripts
+Compresses all of the js files used in hte project and adds them to assets/js
+
+#Build-Styles
+Builds all of the sass files used in hte project and adds them to assets/css
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,40 @@ var plumber = require('gulp-plumber'); // stops gulp from stopping on errors
 var imageMin = require('gulp-imagemin');
 
 
-gulp.task('default',function(){
-    console.log('working');
-})
+/*  Compresses the images used in the project
+_____________________________________________*/
+gulp.task('compress-images', function(){
+    
+    gulp.src('img-tmp/*').pipe(imageMin()).pipe(gulp.dest('assets/img'));
+});
+
+/*  Compresses the JS files used in the project
+_______________________________________________*/
+gulp.task('build-scripts', function(){
+    
+    gulp.src('js/**/*.js').pipe(plumber()).pipe(uglify()).pipe(gulp.dest('assets/js'));
+});
+
+/*  Builds the sass files used in the project
+_______________________________________________*/
+gulp.task('build-styles', function(){
+    
+    // finds all scss files anywhere inside the scss folder. conpiles it, saves it to the css fodler
+    gulp.src('sass/**/*.scss').pipe(plumber()).pipe(sass({style:'compressed'})).pipe(gulp.dest('assets/css'));
+    
+});
+
+
+/*  Watches actives files for updates
+_______________________________________________*/
+gulp.task('watch', function(){
+    
+    // watches all of the files in teh javascript fodler and automatically runs the scripts task
+    gulp.watch('js/*.js',['build-scripts']);
+    
+    // watches all the files  in the scss folder and runs the styles task
+    gulp.watch('sass/*.scss',['build-styles']);
+    
+});
+
+gulp.task('default',['build-styles','build-scripts','watch'])
